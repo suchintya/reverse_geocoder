4dd8a650f8adcad1c7e3d356281e4a0eb329048b
suchintya <suchintya@user.noreply.github.com>
2017-05-15 09:56:46 -0700

    read write from csv

--- convertlatlong.py
+++ convertlatlong.py
@@ -6,10 +6,11 @@ from timezonefinderL import TimezoneFinder
 from pytz import timezone 
 
 DEFAULT_TERM = 1480042675
-DEFAULT_LONGITUDE = -37.76
-DEFAULT_LATITUDE = 122.41
+DEFAULT_LONGITUDE = 122.41
+DEFAULT_LATITUDE = -37.76
 
 def main():
+    print(TimezoneFinder.using_numba())
     parser = argparse.ArgumentParser()
 
     parser.add_argument('-q', '--utc', dest='utc', default=DEFAULT_TERM,
@@ -33,17 +34,40 @@ def main():
                 error.read(),
             )
         )
+    with open('smove.csv') as csvfile:
+        zipreader = csv.reader(csvfile)
+        flag = False
+        count = 0
+        with open('output.csv','w',newline='') as csvout:
+            for row in zipreader:
+                count = count + 1
+                if count == 10:
+                    exit
+                if flag is False:
+                    flag = True
+                    continue
+                lat = float(row[3])
+                lon = float(row[4])
+                utcTim = int(row[0])
+                aware_time = query_local(lat, lon, utcTim)
+                write_row=list(row)
+                write_row.append(str(aware_time))
+                spamwriter = csv.writer(csvout)
+                spamwriter.writerow(write_row)
 
 def query_local(latitude, longitude, utcTime):
-    tf = TimezoneFinder()
-    timezone_name = tf.timezone_at(lng=longitude, lat=latitude)
+    tf = TimezoneFinder()    
+    aware_datetime_in_local = datetime.datetime.now
+    try:
+        timezone_name = tf.timezone_at(lng=longitude, lat=latitude)
 
-    tz = timezone(timezone_name)
-    value = datetime.datetime.fromtimestamp(utcTime)
-    aware_datetime = pytz.utc.localize(value)
-    aware_datetime_in_local = aware_datetime.astimezone(tz)
-
-    print(aware_datetime_in_local)
+        tz = timezone(timezone_name)
+        value = datetime.datetime.fromtimestamp(utcTime)
+        aware_datetime = pytz.utc.localize(value)
+        aware_datetime_in_local = aware_datetime.astimezone(tz)
+    except RuntimeWarning:
+        pass
+    return aware_datetime_in_local
 
 if __name__ == '__main__':
     main()
